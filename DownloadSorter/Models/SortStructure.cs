026188d89ee701add317ead070cc6d04edd37f9f
Involts <100028421+Involts@users.noreply.github.com>
2023-08-05 13:06:18 +0800

    Add project files.

---
+++ DownloadSorter/Models/SortStructure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadSorter.Models
+{
+    public class SortStructure
+    {
+        public string Name { get; set; }
+        public List<Information> Information { get; set; }
+
+    }
+    public class Information
+    {
+        public string Location { get; set; }
+        public string Sortfile { get; set; }
+    }
+    //public class InformationStructure
+    //{
+    //    public List<Information> Information { get; set; }
+    //}
+}
---
+++ DownloadSorter/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DownloadSorter.Services;
+using System.Reflection;
+
+SortManagemnt createSort = new SortManagemnt();
+
+
+Console.Title = "DownloadSorter";
+Console.WriteLine("Download Sorter" + "[" + "v" + Assembly.GetExecutingAssembly().GetName().Version + "]");
+Thread.Sleep(1000);
+createSort.list_Sorts();
+Console.ReadLine();
